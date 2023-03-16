c17872a4630cc5500f19d6214fd386c79359c0ab
dwinrynt <dwinuryantosukatanai@gmail.com>
2023-03-16 11:14:14 +0700

    db_wilayah

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use DB;
 use App\Models\{
     User,
     Kategori
@@ -40,5 +41,38 @@ class DatabaseSeeder extends Seeder
         Kategori::create([
             'nama_kategori' => 'Ekonomi'
         ]);
+
+        // $provinsis = json_decode(file_get_contents('https://ibnux.github.io/data-indonesia/provinsi.json'));
+
+        // foreach ($provinsis as $key => $provinsi) {
+        //     DB::table('provinsis')->insert([
+        //         'id' => $provinsi->id,
+        //         'nama' => $provinsi->nama,
+        //     ]);
+        //     $kabupatens = json_decode(file_get_contents('https://ibnux.github.io/data-indonesia/kabupaten/'. $provinsi->id .'.json'));
+        //     foreach ($kabupatens as $key => $kabupaten) {
+        //         DB::table('kabupatens')->insert([
+        //             'id' => $kabupaten->id,
+        //             'nama' => $kabupaten->nama,
+        //             'provinsi_id' => $provinsi->id
+        //         ]);
+        //         $kecamatans = json_decode(file_get_contents('https://ibnux.github.io/data-indonesia/kecamatan/'. $kabupaten->id .'.json'));
+        //         foreach ($kecamatans as $key => $kecamatan) {
+        //             DB::table('kecamatans')->insert([
+        //                 'id' => $kecamatan->id,
+        //                 'nama' => $kecamatan->nama,
+        //                 'kabupaten_id' => $kabupaten->id
+        //             ]);
+        //             $kelurahans = json_decode(file_get_contents('https://ibnux.github.io/data-indonesia/kelurahan/'. $kecamatan->id .'.json'));
+        //             foreach ($kelurahans as $key => $kelurahan) {
+        //                 DB::table('kelurahans')->insert([
+        //                     'id' => $kelurahan->id,
+        //                     'nama' => $kelurahan->nama,
+        //                     'kecamatan_id' => $kecamatan->id
+        //                 ]);
+        //             }
+        //         }
+        //     }
+        // }
     }
 }
---
+++ db_wilayah/provinsis.sql
@@ -0,0 +1,99 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Mar 16, 2023 at 04:10 AM
+-- Server version: 8.0.29
+-- PHP Version: 7.4.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ukk_pusat_pengaduan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `provinsis`
+--
+
+CREATE TABLE `provinsis` (
+  `id` bigint UNSIGNED NOT NULL,
+  `nama` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `provinsis`
+--
+
+INSERT INTO `provinsis` (`id`, `nama`, `created_at`, `updated_at`) VALUES
+(11, 'ACEH', NULL, NULL),
+(12, 'SUMATERA UTARA', NULL, NULL),
+(13, 'SUMATERA BARAT', NULL, NULL),
+(14, 'RIAU', NULL, NULL),
+(15, 'JAMBI', NULL, NULL),
+(16, 'SUMATERA SELATAN', NULL, NULL),
+(17, 'BENGKULU', NULL, NULL),
+(18, 'LAMPUNG', NULL, NULL),
+(19, 'KEPULAUAN BANGKA BELITUNG', NULL, NULL),
+(21, 'KEPULAUAN RIAU', NULL, NULL),
+(31, 'DKI JAKARTA', NULL, NULL),
+(32, 'JAWA BARAT', NULL, NULL),
+(33, 'JAWA TENGAH', NULL, NULL),
+(34, 'DAERAH ISTIMEWA YOGYAKARTA', NULL, NULL),
+(35, 'JAWA TIMUR', NULL, NULL),
+(36, 'BANTEN', NULL, NULL),
+(51, 'BALI', NULL, NULL),
+(52, 'NUSA TENGGARA BARAT', NULL, NULL),
+(53, 'NUSA TENGGARA TIMUR', NULL, NULL),
+(61, 'KALIMANTAN BARAT', NULL, NULL),
+(62, 'KALIMANTAN TENGAH', NULL, NULL),
+(63, 'KALIMANTAN SELATAN', NULL, NULL),
+(64, 'KALIMANTAN TIMUR', NULL, NULL),
+(65, 'KALIMANTAN UTARA', NULL, NULL),
+(71, 'SULAWESI UTARA', NULL, NULL),
+(72, 'SULAWESI TENGAH', NULL, NULL),
+(73, 'SULAWESI SELATAN', NULL, NULL),
+(74, 'SULAWESI TENGGARA', NULL, NULL),
+(75, 'GORONTALO', NULL, NULL),
+(76, 'SULAWESI BARAT', NULL, NULL),
+(81, 'MALUKU', NULL, NULL),
+(82, 'MALUKU UTARA', NULL, NULL),
+(91, 'P A P U A', NULL, NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `provinsis`
+--
+ALTER TABLE `provinsis`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `provinsis`
+--
+ALTER TABLE `provinsis`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=92;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
